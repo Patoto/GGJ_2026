7585c47153bd66df5d1cf609b5f4670cf2512b8f
Markus Villalobos Corrales <MarkusVC64@gmail.com>
2026-01-31 03:21:36 -0600

    Inputs

--- Assets/Scripts/Managers/InputsManager.cs
+++ Assets/Scripts/Managers/InputsManager.cs
@@ -1,9 +1,31 @@
+using DigitalRubyShared;
+using System;
 using UnityEngine;
 
 namespace GGJ_2026
 {
-	public class InputsManager : Manager
-	{
-		
-	}
+    public class InputsManager : Manager, IEventSubscriberDeclarator
+    {
+        [SerializeField] private SwipeGestureRecognizerComponentScript swipeDownGestureRecognizer;
+
+        public static Action<GestureRecognizer> onSwipeDownGestureEnded;
+
+        public void SubscribeToEnableEvents()
+		{
+            swipeDownGestureRecognizer.Gesture.StateUpdated += OnSwipeDownGestureRecognizerStateUpdated;
+		}
+
+        public void UnsubscribeFromEnableEvents()
+		{
+            swipeDownGestureRecognizer.Gesture.StateUpdated -= OnSwipeDownGestureRecognizerStateUpdated;
+		}
+
+        private void OnSwipeDownGestureRecognizerStateUpdated(GestureRecognizer gestureRecognizer)
+        {
+            if (gestureRecognizer.State == GestureRecognizerState.Ended)
+            {
+                onSwipeDownGestureEnded?.Invoke(gestureRecognizer);
+            }
+        }
+    }
 }
