6b1886e23df4828844e3fc0dad1e75c02e8f8333
Markus Villalobos Corrales <MarkusVC64@gmail.com>
2026-01-31 11:55:51 -0600

    Catch events

---
+++ Assets/Scripts/Handlers/CatchEventsHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ_2026
+{
+	public class CatchEventsHandler : MyMonoBehaviour, IEventSubscriberDeclarator
+	{
+		[SerializeField] private Transform catchEventsParent;
+		
+		private List<CatchEvent> catchEventsList = new();
+
+        public void SubscribeToEnableEvents()
+		{
+			CatchEvent.onFinished += OnCatchEventFinished;
+		}
+
+        public void UnsubscribeFromEnableEvents()
+        {
+			CatchEvent.onFinished -= OnCatchEventFinished;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+			catchEventsList = catchEventsParent.gameObject.GetChildrenWithComponent<CatchEvent>();
+        }
+
+        private void Start()
+        {
+            InvokeNextCatchEvent();
+        }
+
+        private void InvokeNextCatchEvent()
+		{
+			InvokeActionAfterSeconds(StartRandomUnusedCatchEvent, UnityEngine.Random.Range(2.5f, 5f));
+		}
+
+        private void StartRandomUnusedCatchEvent()
+		{
+			catchEventsList.GetUnusedElement(Extensions.UnusedElementsListData<object>.GetOrderType.Random).StartCatchEvent();
+		}
+
+        private void OnCatchEventFinished()
+		{
+			InvokeNextCatchEvent();
+		}
+    }
+}
--- Assets/Scripts/Others/CatchEvents/CatchEvent.cs
+++ Assets/Scripts/Others/CatchEvents/CatchEvent.cs
@@ -1,12 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace GGJ_2026
 {
 	public class CatchEvent : MyMonoBehaviour
 	{
+		[SerializeField] private PlayableDirector playableDirector;
+
         public static Action onStartedWatching;
         public static Action onStoppedWatching;
+        public static Action onFinished;
+
+        public void StartCatchEvent()
+		{
+			playableDirector.Play();
+		}
 
         public void StartWatching()
 		{
@@ -16,6 +25,11 @@ namespace GGJ_2026
 		public void StopWatching()
 		{			
 			onStoppedWatching?.Invoke();
+		}
+
+		public void OnFinished()
+		{
+			onFinished?.Invoke();
 		}
 	}
 }
