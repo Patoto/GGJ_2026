fa60d81a51aec5e24eae858343b42a7f6ef02c16
Markus Villalobos Corrales <MarkusVC64@gmail.com>
2026-01-31 04:22:32 -0600

    Masking strip

--- Assets/Scripts/UI/Objects/MaskingStrip.cs
+++ Assets/Scripts/UI/Objects/MaskingStrip.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using DG.Tweening;
+using DigitalRubyShared;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -6,22 +9,60 @@ namespace GGJ_2026
 {
     public class MaskingStrip : MyMonoBehaviour, IEventSubscriberDeclarator
     {
+        private IEnumerator rollDownCoroutine;
+
         public void SubscribeToEnableEvents()
 		{
+			LevelPointerListener.onPointerDown += OnLevelPointerListenerPointerDown;
 			LevelPointerListener.onPointerDrag += OnLevelPointerListenerPointerDrag;
+			InputsManager.onSwipeDownGestureEnded += OnInputsManagerSwipeDownGestureEnded;
 		}
 
         public void UnsubscribeFromEnableEvents()
         {
+			LevelPointerListener.onPointerDown -= OnLevelPointerListenerPointerDown;
 			LevelPointerListener.onPointerDrag -= OnLevelPointerListenerPointerDrag;
+			InputsManager.onSwipeDownGestureEnded -= OnInputsManagerSwipeDownGestureEnded;
         }
 
         private void OnLevelPointerListenerPointerDrag(PointerEventData pointerEventData)
 		{
 			if (pointerEventData.delta.y < 0)
+            {
+                AddY(pointerEventData.delta.y);
+            }
+        }
+
+        private void AddY(float y)
+        {
+            transform.SetLocalPositionY(Mathf.Max(transform.localPosition.y + y, 10f));
+        }
+
+        private void OnInputsManagerSwipeDownGestureEnded(GestureRecognizer gestureRecognizer)
+        {
+            StopRollDownCoroutine();
+            rollDownCoroutine = StartCoroutine(RollDownCoroutine(gestureRecognizer.VelocityY));
+        }
+
+        private void StopRollDownCoroutine()
+        {
+            StopCoroutine(rollDownCoroutine);
+        }
+
+        private IEnumerator RollDownCoroutine(float velocityY)
+		{
+            float acceleration = -velocityY * 0.5f;
+			while (Mathf.Abs(velocityY) > 0.1f)
 			{
-				transform.SetLocalPositionY(Mathf.Max(transform.localPosition.y + pointerEventData.delta.y, 10f));
+				AddY(velocityY * Time.deltaTime);
+                velocityY += acceleration * Time.deltaTime;
+				yield return null;
 			}
+		}
+
+        private void OnLevelPointerListenerPointerDown(PointerEventData pointerEventData)
+		{
+			StopRollDownCoroutine();
 		}
     }
 }
