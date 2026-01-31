0302683730d319ccc9eeb731cb340c77324b3fa2
Markus Villalobos Corrales <MarkusVC64@gmail.com>
2026-01-31 03:31:08 -0600

    Paw

--- Assets/Scripts/Objects/Paw.cs
+++ Assets/Scripts/Objects/Paw.cs
@@ -11,21 +11,33 @@ namespace GGJ_2026
 		[SerializeField] private Transform downLimitPoint;
 		[SerializeField] private Transform upLimitPoint;
 
-        public void SubscribeToEnableEvents()
+        public void SubscribeToEnableEvents() { }
+
+        public void UnsubscribeFromEnableEvents() { }
+
+		public virtual void SubscribeToInitializeEvents()
 		{
 			PawPointerListener.onPointerDrag += OnInputsPointerListenerPointerDrag;
+			PawPointerListener.onPointerUp += OnPawPointerListenerPointerUp;
 		}
 
-        public void UnsubscribeFromEnableEvents()
-		{			
+		public virtual void UnsubscribeFromInitializeEvents()
+		{
 			PawPointerListener.onPointerDrag -= OnInputsPointerListenerPointerDrag;
+			PawPointerListener.onPointerUp -= OnPawPointerListenerPointerUp;
 		}
 
         private void OnInputsPointerListenerPointerDrag(PointerEventData pointerEventData)
 		{
+			gameObject.SetActive(true);
             float clampedPositionX = Mathf.Clamp(pointerEventData.position.x, leftLimitPoint.position.x, rightLimitPoint.position.x);
             float clampedPositionY = Mathf.Clamp(pointerEventData.position.y, downLimitPoint.position.y, upLimitPoint.position.y);
             transform.position = new Vector2(clampedPositionX, clampedPositionY);
+		}
+
+        private void OnPawPointerListenerPointerUp(PointerEventData pointerEventData)
+		{
+			gameObject.SetActive(false);
 		}
     }
 }
--- Assets/Scripts/UI/Others/PawPointerListener.cs
+++ Assets/Scripts/UI/Others/PawPointerListener.cs
@@ -9,20 +9,28 @@ namespace GGJ_2026
 		[SerializeField] private PointerListener pointerListener;
 
         public static Action<PointerEventData> onPointerDrag;
+		public static Action<PointerEventData> onPointerUp;
 
         public void SubscribeToEnableEvents()
 		{
 			PointerListener.onPointerDrag += OnPointerListenerPointerDrag;
+			PointerListener.onPointerUp += OnPointerListenerPointerUp;
 		}
 
         public void UnsubscribeFromEnableEvents()
         {
 			PointerListener.onPointerDrag -= OnPointerListenerPointerDrag;
+			PointerListener.onPointerUp -= OnPointerListenerPointerUp;
         }
 
         private void OnPointerListenerPointerDrag(PointerListener pointerListener, PointerEventData pointerEventData)
 		{
 			onPointerDrag?.Invoke(pointerEventData);
+		}
+
+        private void OnPointerListenerPointerUp(PointerListener pointerListener, PointerEventData pointerEventData)
+		{
+			onPointerUp?.Invoke(pointerEventData);
 		}
     }
 }
