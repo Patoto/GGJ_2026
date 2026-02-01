46aa243e9963e30da80607d12a0b4c984e90b001
Markus Villalobos Corrales <MarkusVC64@gmail.com>
2026-02-01 08:24:33 -0600

    Masking

---
+++ Assets/Scripts/UI/Objects/MaskingTape.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ_2026
+{
+	public class MaskingTape : MyMonoBehaviour, IEventSubscriberDeclarator
+	{
+		private enum State
+		{
+			Full,
+			Middle,
+			Low,
+			Empty
+		}
+
+		[SerializeField] private Image image;
+		[SerializeField] private Sprite fullSprite;
+		[SerializeField] private Sprite middleSprite;
+		[SerializeField] private Sprite lowSprite;
+		[SerializeField] private Sprite emptySprite;
+
+		private float tapePercentageLeftAmount = 100f;
+
+        public void SubscribeToEnableEvents()
+		{
+			MaskingStrip.onAddedY += OnMaskingStripAddedY;
+		}
+
+        public void UnsubscribeFromEnableEvents()
+        {
+			MaskingStrip.onAddedY -= OnMaskingStripAddedY;
+        }
+
+        private void OnMaskingStripAddedY(float y)
+		{
+			tapePercentageLeftAmount -= Mathf.Abs(y) * 0.1f;
+			if (tapePercentageLeftAmount <= 0f)
+			{
+				SetState(State.Empty);
+			}
+			else if (tapePercentageLeftAmount <= 0.33f)
+			{
+				SetState(State.Low);
+			}
+			else if (tapePercentageLeftAmount <= 0.66f)
+			{
+				SetState(State.Middle);
+			}
+			else
+			{
+				SetState(State.Full);
+			}
+		}
+
+        private void SetState(State state)
+		{
+            switch (state)
+            {
+                case State.Full:
+                    SetSprite(fullSprite);
+                    break;
+                case State.Middle:
+                    SetSprite(middleSprite);
+                    break;
+                case State.Low:
+                    SetSprite(lowSprite);
+                    break;
+                case State.Empty:
+                    SetSprite(emptySprite);
+                    break;
+            }
+        }
+
+        private void SetSprite(Sprite sprite)
+        {
+            image.sprite = sprite;
+        }
+    }
+}
