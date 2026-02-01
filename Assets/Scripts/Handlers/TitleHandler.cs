3d1f616257c3c3e6f892674fa5caa98cd9f68411
Markus Villalobos Corrales <MarkusVC64@gmail.com>
2026-02-01 13:16:26 -0600

    Haptics

--- Assets/Scripts/Handlers/TitleHandler.cs
+++ Assets/Scripts/Handlers/TitleHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using TapticPlugin;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -35,6 +36,7 @@ namespace GGJ_2026
 
         private void OnLevelPointerListenerPointerUp(PointerEventData pointerEventData)
         {
+			TapticManager.Impact(ImpactFeedback.Medium);
             Deactivate();
         }
 
